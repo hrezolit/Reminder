6b8af6007c929bed05e91407f5a8321531c87df7
Nik <hrezolitaz@gmail.com>
2023-08-05 10:11:46 +0600

    add indicator of the complete or incomplete status for each reminder

--- Reminder/Views/ListViewController/ReminderListViewController+DataSource.swift
+++ Reminder/Views/ListViewController/ReminderListViewController+DataSource.swift
@@ -16,16 +16,33 @@ extension ReminderListViewController {
     // cell registration and configuration
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
         let reminder = Reminder.sampleData[indexPath.item]
-        var contentConfiguration = cell.defaultContentConfiguration()
         
+        var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
         
-        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+        doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+        cell.accessories = [
+            .customView(configuration: doneButtonConfiguration),
+            .disclosureIndicator(displayed: .always)
+        ]
         
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
         backgroundConfiguration.backgroundColor = .todayListCellBackground
         cell.backgroundConfiguration = backgroundConfiguration
+    }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbol = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbol, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        
+        return UICellAccessory.CustomViewConfiguration(customView: button,
+                                                       placement: .leading(displayed: .always))
     }
 }
