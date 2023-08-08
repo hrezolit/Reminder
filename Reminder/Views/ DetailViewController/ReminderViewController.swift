d1ab45b04fddd853b4b394b21646f817858fd117
Nik <hrezolitaz@gmail.com>
2023-08-08 14:25:44 +0600

    add custom configuration for the list cells in the reminder detail view

--- Reminder/Views/ DetailViewController/ReminderViewController.swift
+++ Reminder/Views/ DetailViewController/ReminderViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ReminderViewController: UICollectionViewController {
+    
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    private var dataSource: DataSource!
+    
     var reminder: Reminder
     
     init(reminder: Reminder) {
@@ -20,6 +24,30 @@ class ReminderViewController: UICollectionViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView,
+                                                                   indexPath: IndexPath,
+                                                                   itemIdentifier: Row) in
+            
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                for: indexPath,
+                                                                item: itemIdentifier)
+        }
+    }
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        
+        cell.contentConfiguration = contentConfiguration
     }
     
     func text(for row: Row) -> String? {
