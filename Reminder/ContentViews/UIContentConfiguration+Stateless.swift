745f6882f7e27e1ef9344e985996edaf11744678
Nik <hrezolitaz@gmail.com>
2023-08-12 08:55:09 +0600

    Conform to the content view protocol

---
+++ Reminder/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,15 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Reminder
+//
+//  Created by Nikita on 12/8/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        
+        return self
+    }
+}
