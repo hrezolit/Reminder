af204e0df715a03ffd361d0ba2bec23a4ef85b02
Nik <hrezolitaz@gmail.com>
2023-08-15 09:32:19 +0600

    fetch reminders asynchronously

---
+++ Reminder/Models/EKEventStore+AsyncFetch.swift
@@ -0,0 +1,25 @@
+//
+//  EKEventStore+AsyncFetch.swift
+//  Reminder
+//
+//  Created by Nikita on 15/8/23.
+//
+
+import Foundation
+import EventKit
+
+extension EKEventStore {
+    func reminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
+        
+        try await withCheckedThrowingContinuation { continuation in
+            fetchReminders(matching: predicate) { reminders in
+                if let reminders {
+                    continuation.resume(returning: reminders)
+                } else {
+                    continuation.resume(returning: ReminderError.failedReadingReminders)
+                }
+                
+            }
+        }
+    }
+}
