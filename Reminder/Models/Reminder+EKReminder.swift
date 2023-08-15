6134ee973c75a3da394d5c536161e56b46e05a22
Nik <hrezolitaz@gmail.com>
2023-08-15 09:40:56 +0600

    fix

--- Reminder/Models/Reminder+EKReminder.swift
+++ Reminder/Models/Reminder+EKReminder.swift
@@ -9,8 +9,14 @@ import Foundation
 import EventKit
 
 extension Reminder {
+    
     init(with ekReminder: EKReminder) throws {
         guard let dueDate = ekReminder.alarms?.first?.absoluteDate else { throw ReminderError.reminderHasNoDueDate }
+        
         id = ekReminder.calendarItemIdentifier
+        title = ekReminder.title
+        self.dueDate = dueDate
+        notes = ekReminder.notes
+        isComplete = ekReminder.isCompleted
     }
 }
